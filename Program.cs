d7156e010bfa17cb7c5a61f7c6f073fcfb64a9f3
urza <urzidlo@gmail.com>
2010-06-13 16:04:21 -0700

    lost in git

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+
+namespace ProjectEulerCsharp
+{
+    class Program
+    {
+        [STAThread] 
+        static void Main(string[] args)
+        {
+            var result = Problems._5();
+
+            Console.WriteLine(result);
+            Console.Read();
+            Clipboard.SetData(DataFormats.Text, (Object)result);
+
+        }
+    }
+}
