412b94ca23726c123f5ab6b8470c49532dfb3f28
urza <urzidlo@gmail.com>
2010-06-21 23:38:31 +0200

    "To" range generator as replace for Enumerable.Range

--- Extensions.cs
+++ Extensions.cs
@@ -12,8 +12,17 @@ namespace ProjectEulerCsharp
             foreach (var item in @enum) mapFunction(item);
         }
     }
+
     public static class Int32Extensions
     {
+        public static IEnumerable<int> To(this int first, int last)
+        {
+            for (int i = first; i <= last; i++)
+            {
+                yield return i;
+            }
+        }
+
         public static bool IsPalindrome(this int val)
         {
             var digits = val.ToString().ToCharArray();
