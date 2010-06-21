b1ab80c26bec02e7149a465173eddb40e6b01c25
urza <urzidlo@gmail.com>
2010-06-21 23:38:47 +0200

    6th solution

--- Problems.cs
+++ Problems.cs
@@ -114,5 +114,25 @@ namespace ProjectEulerCsharp
         }
         #endregion
 
+        #region problem 6
+        /// <summary>
+        /// The sum of the squares of the first ten natural numbers is,
+        /// 12 + 22 + ... + 102 = 385
+        /// The square of the sum of the first ten natural numbers is,
+        /// (1 + 2 + ... + 10)2 = 552 = 3025
+        /// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+        /// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+        /// </summary>
+        /// <returns></returns>
+        public static int _6()
+        {
+            var sum100 = 1.To(100).Sum();
+
+            var sumOfSquares = 1.To(100).Select(x => x * x).Sum();
+
+            return (sum100 * sum100) - sumOfSquares;
+        }
+        #endregion
+
     }
 }
